e379209485392b34987e66aec49352639dd4105a
Arad Jahangiri <aradjahangiri84@gmail.com>
2023-01-19 01:40:48 +0330

    ROUTES DONE

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,3 @@
-//pages
-import Home from "./pages/Home";
-import Destination from "./pages/Destination";
-import Crew from "./pages/Crew";
-import Technology from "./pages/Technology";
-
 //routes interface
 
 interface IRoute {
